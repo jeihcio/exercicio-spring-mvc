a5d1c058c6387a1c98b1493582dba3a7f6c9fd1d
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-05-17 20:36:24 -0300

    Pegando os produtos pelo o banco de dados

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -3,6 +3,11 @@ package br.com.alura.mvc.mudi.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContexts;
+import javax.persistence.Query;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,19 +16,17 @@ import br.com.alura.mvc.mudi.model.Pedido;
 
 @Controller
 public class HomeController {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
 
 	@GetMapping("/home")
 	public String home(Model model) {
-		Pedido pedido = new Pedido();
 		
-		pedido.setNomeProduto("Xiaomi Redmi Note 8");
-		pedido.setUrlImagem("https://m.media-amazon.com/images/I/51wgmCYDFML._AC_UL320_.jpg");
-		pedido.setUrlProduto("https://www.amazon.com.br/Celular-Xiaomi-Vers%C3%A3o-Global-Space/dp/B07Y9ZHLXW/ref=sr_1_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=xiaomi+redmi+note+8&qid=1618595292&sr=8-1");
-		pedido.setDescricao("Uma descrição qualquer para o pedido");
+		Query query = entityManager.createQuery("Select p From Pedido p", Pedido.class);
+		List<Pedido> pedidos = query.getResultList();
 		
-		List<Pedido> pedidos = Arrays.asList(pedido);
-		model.addAttribute("pedidos", pedidos);
-		
+		model.addAttribute("pedidos", pedidos);		
 		return "home";
 	}
 }
