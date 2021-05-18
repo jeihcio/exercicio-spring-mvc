703666f61fc1f3c2ca9ef0590d50d8df8bdbf888
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-05-18 10:29:03 -0300

    Adicionando o topo do site

--- src/main/java/br/com/alura/mvc/mudi/Repository/PedidosRepository.java
+++ src/main/java/br/com/alura/mvc/mudi/Repository/PedidosRepository.java
@@ -1,22 +1,10 @@
 package br.com.alura.mvc.mudi.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import br.com.alura.mvc.mudi.model.Pedido;
 
 @Repository
-public class PedidosRepository {
-	
-	@PersistenceContext
-	private EntityManager entityManager;
-	
-	public List<Pedido> recuperaTodosOsPedidos(){
-		Query query = entityManager.createQuery("Select p From Pedido p", Pedido.class);
-		return query.getResultList();
-	}
+public interface PedidosRepository extends JpaRepository<Pedido, Long> {
 }
