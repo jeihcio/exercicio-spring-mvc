2f62f93dc1d2a7b7cf3e2d261d688c38a260d2d2
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-05-24 18:39:26 -0300

    Criando o link de login e logout

--- src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
+++ src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
@@ -16,7 +16,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll();
+		http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and()
+				.logout().logoutUrl("/logout");
 	}
 
 	@Bean
