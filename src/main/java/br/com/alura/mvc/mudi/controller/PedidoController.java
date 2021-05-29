54e4e8a62f2dadb8dae4b6abe2021eb459d95baf
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-05-29 17:23:59 -0300

    Removendo imports desnecessários

--- src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
@@ -3,7 +3,6 @@ package br.com.alura.mvc.mudi.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
