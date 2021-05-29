35b21a7ad8115fbb34c29d620c8e8ce1c57bb83b
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-05-29 18:40:56 -0300

    criando menu de navegação

--- src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
+++ src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
@@ -21,9 +21,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login")
-				.defaultSuccessUrl("/usuario/pedido", true).permitAll().and().logout().logoutUrl("/logout").and().csrf()
-				.disable();
+		http.authorizeRequests().antMatchers("/home/**").permitAll().anyRequest().authenticated().and().formLogin()
+				.loginPage("/login").defaultSuccessUrl("/usuario/pedido", true).permitAll().and()
+				.logout(logout -> {
+					logout.logoutUrl("/logout")
+					.logoutSuccessUrl("/home");
+				})
+				.csrf().disable();
 	}
 
 	@Override
