40aa3e15271d04c2f909bc2e8028e4ac66a796ae
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-06-01 10:46:38 -0300

    Fazendo o interceptador funcionar

--- src/main/java/br/com/alura/mvc/mudi/WebConfig.java
+++ src/main/java/br/com/alura/mvc/mudi/WebConfig.java
@@ -1,10 +1,12 @@
 package br.com.alura.mvc.mudi;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import br.com.alura.mvc.mudi.interceptor.InterceptadorDeAcessos;
 
+@Configuration
 public class WebConfig extends WebMvcConfigurationSupport {
 	
 	@Override
